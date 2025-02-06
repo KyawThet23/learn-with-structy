e388425227e32d2239e50c7bb513034308ba9c7c
kyawthethtun <kyawthe235@gmail.com>
2025-02-06 21:31:01 +0630

    SOL2

--- beginnerRecursion/sumOfLength.java
+++ beginnerRecursion/sumOfLength.java
@@ -2,13 +2,14 @@ package beginnerRecursion;
 
 public class sumOfLength {
     public static void main(String[] args) {
-        System.out.println(sumOfLength("hello"));
+        String[] str = {"hello","world"};
+        System.out.println(sumLength(str,0));
     }
 
-    public static int sumOfLength(String str) {
-        if (str.equals("")) {
+    public static int sumLength(String[] str,int index) {
+        if (index >= str.length ) {
             return 0;
         }
-        return 1 + sumOfLength(str.substring(1));
+        return str[index].length() + sumLength(str,index+1);
     }
 }
