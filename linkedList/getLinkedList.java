efb7770b9fa6bdb58a6e3f2a25d8d98c83736b7e
kyawthethtun <kyawthe235@gmail.com>
2025-02-11 22:40:01 +0630

    get linked list

--- linkedList/getLinkedList.java
+++ linkedList/getLinkedList.java
@@ -1,25 +1,29 @@
 package linkedList;
 
-public class warnup {
-
+public class getLinkedList {
     public static void main(String[] args) {
-
         Node<Integer> a = new Node<>(10);  
         Node<Integer> b = new Node<>(20); 
         Node<Integer> c = new Node<>(30);
         
         a.next = b;
         b.next = c;
-        System.out.println(a.next.value);
+        System.out.println(solution(a, 2));
     }
 
-    private static void list(Node head){
-        Node current = head;
+    private static <T> T solution(Node<T> head, int index) {
+        int count = 0;
+        Node<T> current = head;
         while (current != null) {
-            System.out.println(current.value);
+            if (count == index) {
+                return current.value;
+            }
             current = current.next;
+            count += 1;
         }
+        return null;
     }
+
 }
 
 class Node<T> {
@@ -27,7 +31,7 @@ class Node<T> {
     T value;
     Node next;
 
-    public Node(T value){
+    public Node(T value) {
         this.value = value;
         this.next = null;
     }
